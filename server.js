44c5ce304f9f1a89e0d5073b483a549630fec661
AhmedSami36 <119262462+AhmedSami36@users.noreply.github.com>
2024-08-10 20:55:00 +0300

    setup server.js

--- server.js
+++ server.js
@@ -25,3 +25,35 @@ const corsOptions = {
     methods: 'GET,HEAD,OPTIONS,POST,PUT',
     allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   };
+  app.use(cors(corsOptions));
+
+// Middleware
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(session({
+    secret: config.jwtSecret,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+app.use(helmet());
+app.use(extractToken);
+app.use(errorMiddleware);
+
+//Routes
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI_REMOTE).then(() => {
+    console.log('MongoDB connection successfully');
+}).catch(err => {
+    console.log(err);
+    console.log('MongoDB connection failed');
+    process.exit(1);
+});
+
+// Start the server
+const PORT = config.port || 3002;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
