4388e6de8d9eb9588f42448c9b76b1c7a9ff6b19
ym20206098 <134073265+YusufMahmoud@users.noreply.github.com>
2024-10-04 16:02:38 +0300

    Update UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -22,11 +22,11 @@ const userSchema = new mongoose.Schema({
     type: {
       type: String, 
       enum: ['Point'], 
-      required: true
+      required: false
     },
     coordinates: {
       type: [Number], 
-      required: true
+      required: false
     }
   }
 });
